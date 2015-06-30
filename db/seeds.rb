264443e342b8778cbfa190b19152c7dc43814ff9
Miranda \"don't fk with my github picture\" Pakozdi <super.y4n@gmail.com>
2015-06-30 14:48:32 -0700

    complete seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -17,41 +17,37 @@ require 'faker'
             email: Faker::Internet.email)
 end
 
-Activity.create(name: )
-
-Practice Meditation
-Reconnect with an Old Friend
-Go on a Picnic
-Fly First Class
-Go Skydiving
-Go on a Cruise
-Travel to a Different Country
-Visit a Castle
-Learn a New Language
-Travel to a Country
-Achieve a Fitness Goal
-Run a Marathon
-Learn a New Sport
-Scuba Diving
-Climb a Mountain
-Perform a Kind Act
-Be a Mentor
-Volunteer Work
-Talk to a Stranger
-Go on a Road Trip
-Go Backpacking
-Learn a New Craft
-Write a Book
-Conquer Biggest Fear
-Learn Sign Language
-Publish a Book
-Own a Pet
-Public Speaking
-Throw a Mega Party
-Get a Makeover
-Take a Class
-Go on a Blind Date
-Learn an Instrument
-Learn a Martial Art
-Take up Dancing
+Activity.create(name: Practice Meditation, description: Faker::Lorem.sentence)
+Activity.create(name: Reconnect with an Old Friend, description: Faker::Lorem.sentence)
+Activity.create(name: Go on a Picnic, description: Faker::Lorem.sentence)
+Activity.create(name: Fly First Class, description: Faker::Lorem.sentence)
+Activity.create(name: Go Skydiving, description: Faker::Lorem.sentence)
+Activity.create(name: Go on a Cruise, description: Faker::Lorem.sentence)
+Activity.create(name: Travel to a Different Country, description: Faker::Lorem.sentence)
+Activity.create(name: Visit a Castle, description: Faker::Lorem.sentence)
+Activity.create(name: Learn a New Language, description: Faker::Lorem.sentence)
+Activity.create(name: Achieve a Fitness Goal, description: Faker::Lorem.sentence)
+Activity.create(name: Run a Marathon, description: Faker::Lorem.sentence)
+Activity.create(name: Learn a New Sport, description: Faker::Lorem.sentence)
+Activity.create(name: Scuba Dive, description: Faker::Lorem.sentence)
+Activity.create(name: Climb a Mountain, description: Faker::Lorem.sentence)
+Activity.create(name: Perform a Kind Act, description: Faker::Lorem.sentence)
+Activity.create(name: Be a Mentor, description: Faker::Lorem.sentence)
+Activity.create(name: Volunteer Work, description: Faker::Lorem.sentence)
+Activity.create(name: Talk to a Stranger, description: Faker::Lorem.sentence)
+Activity.create(name: Go on a Road Trip, description: Faker::Lorem.sentence)
+Activity.create(name: Go Backpacking, description: Faker::Lorem.sentence)
+Activity.create(name: Learn a New Craft, description: Faker::Lorem.sentence)
+Activity.create(name: Write a Book, description: Faker::Lorem.sentence)
+Activity.create(name: Conquer Biggest Fear, description: Faker::Lorem.sentence)
+Activity.create(name: Learn Sign Language, description: Faker::Lorem.sentence)
+Activity.create(name: Publish a Book, description: Faker::Lorem.sentence)
+Activity.create(name: Own a Pet, description: Faker::Lorem.sentence)
+Activity.create(name: Public Speaking, description: Faker::Lorem.sentence)
+Activity.create(name: Throw a Mega Party, description: Faker::Lorem.sentence)
+Activity.create(name: Get a Makeover, description: Faker::Lorem.sentence)
+Activity.create(name: Go on a Blind Date, description: Faker::Lorem.sentence)
+Activity.create(name: Learn an Instrument, description: Faker::Lorem.sentence)
+Activity.create(name: Learn a Martial Art, description: Faker::Lorem.sentence)
+Activity.create(name: Take up Dancing, description: Faker::Lorem.sentence)
 
