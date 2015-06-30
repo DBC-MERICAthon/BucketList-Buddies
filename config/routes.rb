b81f48ca02587fdee0de4171bfdb8fb065b968c0
miniyakkos <ayakokurushima@gmail.com>
2015-06-30 15:40:53 -0700

    start work dashboard page

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'users#index'
+  root 'users#main'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
@@ -14,6 +14,7 @@ Rails.application.routes.draw do
 
   # Example resource route (maps HTTP verbs to controller actions automatically):
   #   resources :products
+  resources :users
 
   # Example resource route with options:
   #   resources :products do
