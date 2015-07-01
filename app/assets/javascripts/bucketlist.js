140128935cce385aede68be08669f51b61a525a1
Stephanie Lopez <stephanielopez.sf@gmail.com>
2015-06-30 22:30:56 -0700

    add jquery and ajax for new activity in bucket list

---
+++ app/assets/javascripts/bucketlist.js
@@ -0,0 +1,28 @@
+$(document).ready(function() {
+
+
+  $('#add_activity').on("click", function(event) {
+    event.preventDefault();
+    $('#new_activity').show();
+    $(this).hide();
+  });
+
+  $('#new_activity').on("submit", function(event) {
+    event.preventDefault();
+    $.ajax({url: $(this).attr("action"),
+            method: "POST",
+            data: $(this).serialize()
+            }).done(function(serverdata) {
+              // console.log("success!!!!");
+              var newDiv = $('.all-activities .activity:first-child').clone();
+              newDiv.find(".activity-name").find('a').attr('href', '/activities/' + serverdata.id);
+              newDiv.find(".activity-name").find('a').text(serverdata.name);
+              newDiv.find(".activity-description").text(serverdata.description);
+              newDiv.appendTo(".all-activities");
+            }).fail(function(serverdata) {
+              alert("failll");
+            })
+
+  });
+
+})
