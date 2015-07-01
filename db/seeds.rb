ec59b1bf50dac56a1fbce4dfbcce4963700cc935
Mike Farr <mikefarr@mac.com>
2015-06-30 22:38:33 -0700

    reverted seed, now can find a list of everyone who shares interests with a user and what those interests are.

--- db/seeds.rb
+++ db/seeds.rb
@@ -52,47 +52,7 @@ Activity.create(name: 'Learn a Martial Art', description: Faker::Lorem.sentence)
 Activity.create(name: 'Take up Dancing', description: Faker::Lorem.sentence)
 
 
-activities = [
-    'Practice Meditation',
-    'Reconnect with an Old Friend',
-    'Go on a Picnic',
-    'Fly Firstbin/ Class',
-    'Go Skydiving',
-    'Go on a Cruise',
-    'Travel to a Different Country',
-    'Visit a Castle',
-    'Learn a New Language',
-    'Achieve a Fitness Goal',
-    'Run a Marathon',
-    'Learn a New Sport',
-    'Scuba Dive',
-    'Climb a Mountain',
-    'Perform a Kind Act',
-    'Be a Mentor',
-    'Volunteer Work',
-    'Talk to a Stranger',
-    'Go on a Road Trip',
-    'Go Backpacking',
-    'Learn a New Craft',
-    'Write a Book',
-    'Conquer Biggest Fear',
-    'Learn Sign Language',
-    'Publish a Book',
-    'Own a Pet',
-    'Throw a Mega Party',
-    'Get a Makeover',
-    'Go on a Blind Date',
-    'Learn an Instrument',
-    'Learn a Martial Art',
-    'Take up Dancing']
 
-description = "Et voluptates et autem. Ut a eum minima placeat et"
-# users = User.all
-# users.each do |user|
-#   temp_activities = activities.shuffle.take(12)
-#   temp_activities.each do |activity|
-#     UserActivity.create(name: activity, description: description.split(' ').shuffle.join(' '))
-#   #  user.activities.create(name: activity, description: description.split(' ').shuffle.join(' '))
 
 User.all.each do |user|
   10.times do
